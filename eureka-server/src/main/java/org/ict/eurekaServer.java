bba57f301d2d2b2e81cb4011d76d5f1da0c07226
sauron <1369130123qq@gmail.com>
2021-04-06 17:00:24 +0800

    first commit

---
+++ eureka-server/src/main/java/org/ict/eurekaServer.java
@@ -0,0 +1,16 @@
+package org.ict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class eurekaServer {
+
+    public static void main(String[] args) {
+            SpringApplication.run(eurekaServer.class, args);
+    }
+
+
+}
---
+++ eureka-server/src/test/java/com/ict/gtproject/GtprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ict.gtproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
